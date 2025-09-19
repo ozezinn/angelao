167fb9781b23c87c1ad28f3c84b639616b37759f
ozezinn <vitindaregulag@gmail.com>
2025-09-19 16:26:33 -0300

    teste conexao apu de erros

--- app/model/conexao.php
+++ app/model/conexao.php
@@ -6,6 +6,7 @@ class Conexao {
 
     public function conectar()
     {
+        // Pega as credenciais que o Railway fornece
         $host = getenv('MYSQL_HOST');
         $port = getenv('MYSQL_PORT');
         $dbname = getenv('MYSQL_DATABASE');
@@ -13,16 +14,13 @@ class Conexao {
         $senha = getenv('MYSQL_PASSWORD');
 
         try {        
-            // A string de conexão foi atualizada para usar a PORTA e todas as outras variáveis
             $dsn = "mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname;
             $this->pdo = new PDO($dsn, $user, $senha);
-
-            // Configura o PDO para lançar exceções em caso de erro (boa prática)
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch (PDOException $e){
-            // Em um site de produção, é melhor não mostrar o erro detalhado
-            die("ERRO DE CONEXÃO: Não foi possível conectar ao banco de dados.");
+            // MUDANÇA IMPORTANTE: AGORA VAMOS MOSTRAR O ERRO REAL
+            die("ERRO DETALHADO DO PDO: " . $e->getMessage());
         }
 
         return $this->pdo;
